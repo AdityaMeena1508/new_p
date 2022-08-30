badb11a4c8e3cf2cf36bbf34aa902287241c99f4
Aditya <adityameena398@gmail.com>
2022-08-30 12:47:09 +0530

    this is second

--- myapp/models.py
+++ myapp/models.py
@@ -1,14 +1,12 @@
+
 from django.db import models
 
-# import uuid
+import uuid
 
 # Create your models here.
-class myapp(models.Model):
-    # name_id=models.UUIDField(primary_key=True, default=uuid.uuid4)
-    name = models.CharField(max_length=200)
-    age = models.IntegerField()
-    course = models.CharField(max_length=200)
-
-
+class Musician(models.Model):
+    first_name = models.CharField(max_length=200, primary_key=True, unique=True, default=True)
+    last_name = models.CharField(max_length=200, default=True)
     def __str__(self) -> str:
-        return self.name
+        
+        return self.first_name , self.last_name
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Musician
 
 # Create your views here.
 
@@ -17,10 +18,10 @@ def Emp3(request):
     return render (request, 'contactus.html')
 
 def Registration_form(request):
-    # if request.method=="POST":
-    #     fname=request.POST['firstname']
-    #     lname=request.POST['lastname']
-    #     mus=Musician(first_name=fname, last_name=lname)
-    #     mus.save()
-
+    if request.method=="POST":
+        fname=request.POST['firstname']
+        lname=request.POST['lastname']
+        mus=Musician(last_name=lname,first_name=fname)
+        mus.save()
+        mus = Musician.objects.all().order_by('first_name').values()
     return render(request, 'registration_form.html',{'key':'registration'})
